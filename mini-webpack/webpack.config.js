5ee2906bce7edd095df7f3f60849fdb689cc618e
caojunjie <1301239018@qq.com>
2025-08-05 18:12:18 +0800

    opt: config

--- mini-webpack/webpack.config.js
+++ mini-webpack/webpack.config.js
@@ -1,25 +1,9 @@
 // webpack配置文件
 const path = require("path");
-const MyPlugin = require("./plugins/my-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const baseConfig = require('./webpack.kss.config');
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      { test: /\.css$/, use: ["./loaders/css-loader"] }, // 自定义css-loader
-    ],
-  },
-  plugins: [new MyPlugin(), new CopyWebpackPlugin({
-    patterns: [
-      { from: 'src/index.html', to: 'index.html' }
-    ]
-  })],
-
+  ...baseConfig,
   // webpack 测试配置，暂不实现
   mode: "development",
   devServer: {
--- mini-webpack/webpack.kss.config.js
+++ mini-webpack/webpack.kss.config.js
@@ -1,23 +1,23 @@
 // webpack配置文件
-const path = require('path');
-const MyPlugin = require('./plugins/my-plugin');
+const path = require("path");
+const MyPlugin = require("./plugins/my-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'kss_bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
-      { test: /\.css$/, use: ['./loaders/css-loader'] } // 自定义css-loader
-    ]
+      { test: /\.css$/, use: ["./loaders/css-loader"] }, // 自定义css-loader
+    ],
   },
-  plugins: [new MyPlugin()],
-
-  // webpack 测试配置，暂不实现
-  // 关闭 代码压缩
-  optimization: {
-    minimize: false
-  }
+  plugins: [
+    new MyPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/index.html", to: "index.html" }],
+    }),
+  ],
 };
